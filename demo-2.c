c78a22a4a074c2e5d9d157be72312caf5dfce360
huanghai <dusksoft@qq.com>
2016-08-24 18:20:06 +0800

    该死的编码问题

--- demo-2.c
+++ demo-2.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ï»¿#include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
@@ -6,15 +6,15 @@ int main(void) {
 	
 	system("mode con cols=150 lines=30");
 	
-	printf("\t©°©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©´\n");
-	printf("\t©¦                                  ©¦\n");
-	printf("\t©¦            ¾Å¾Å³Ë·¨±í            ©¦\n");
-	printf("\t©¦                                  ©¦\n");
-	printf("\t©¸©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¼\n");
+	printf("\tâ”Œâ”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”\n");
+	printf("\tâ”‚                                  â”‚\n");
+	printf("\tâ”‚            ä¹ä¹ä¹˜æ³•è¡¨            â”‚\n");
+	printf("\tâ”‚                                  â”‚\n");
+	printf("\tâ””â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”˜\n");
 	
 	for(i = 1; i <= 9; i++) {
 		for(y = 1; y <= i; y++)
-			printf("%d ¡Á %d = %d\t", y, i, (i * y));
+			printf("%d Ã— %d = %d\t", y, i, (i * y));
 		printf("\n\n");	
 	}
 	
