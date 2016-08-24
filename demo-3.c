26964324853452d20478fe706d8f3c20a36439b3
huanghai <dusksoft@qq.com>
2016-08-24 20:04:38 +0800

    添加一个文件

---
+++ demo-3.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	int len, i, *array;
+	puts("Please type the array length:");
+	scanf("%d", &len);
+	array = (int *)calloc(sizeof(int), len);
+	for(i = 0; i < len; i++) {
+		array[i] = i;
+	}
+	for(i = 0; i < len; i++) {
+		printf("array[%d]=%d\t", i, array[i]);
+	}
+	free(array);
+	return 0;
+}
